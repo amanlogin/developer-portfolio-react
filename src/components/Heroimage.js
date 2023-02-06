3d124e49d40da98b773241099e0aa72f21d39d06
Aman Kumar <aman_sinha@live.com>
2023-02-07 04:53:47 +0530

    hero page done

---
+++ src/components/Heroimage.js
@@ -0,0 +1,28 @@
+import "./heroimagestyles.css";
+import React from "react";
+import IntroImg from "../assets/intro-background.jpg";
+import { Link } from "react-router-dom";
+
+const Heroimage = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="into-img" src={IntroImg} alt="introImg" />
+      </div>
+      <div className="content">
+        <p>Hi, I'M DEVELOPER</p>
+        <h1>FULLSTACK DEVELOPER</h1>
+        <div>
+          <Link to="/project" className="btn">
+            Projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Heroimage;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Heroimage from "../components/Heroimage";
 
 const home = () => {
   return (
     <div>
       <NavBar />
+      <Heroimage />
     </div>
   );
 };
